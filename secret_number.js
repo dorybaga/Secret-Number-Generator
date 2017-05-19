6af07cea588281fcac2ac12e5fd9cb521dd98501
Dory Baga <dorybaga@Dorys-MacBook-Pro.local>
2017-05-18 19:12:50 -1000

    passed all tests

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,18 @@
 'use strict';
 module.exports = function() {
-  
+
+
+var secretNumber = (function(){
+  var myNum = Math.floor((Math.random() * 1000000) + 0);
+  return function(){
+    return myNum;
+  };
+})();
+
+return secretNumber;
+
 };
+
+
+
+
